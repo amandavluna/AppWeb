5c03c151a95216642bc234157ebace843e834191
amandavluna <amandavitorialuna@gmai.com>
2024-01-26 09:55:45 -0300

    final

--- Model/BancoContato.php
+++ Model/BancoContato.php
@@ -1,30 +1,31 @@
 <?php
 function inserirContato($conexao,$nomeContato,$foneContato){
-    $query = "insert into contatotb(nomeContato,foneContato)values('{$nomeContato}','{$foneContato}')";
+    $query = "insert into contatostb(nomeContato,foneContato)values('{$nomeContato}','{$foneContato}')";
     return mysqli_query($conexao,$query);
 }
 
 function buscarContatoNome($conexao,$nomeContato){
-    $query = "Select * from contatotb where nomeContato like '%{$nomeContato}%'";
+    $query = "Select * from contatostb where nomeContato like '%{$nomeContato}%'";
     $result = mysqli_query($conexao,$query);
     return $result;
 }
 
-function buscarContatoID($conexao,$idContato){
-    $query = "Select * contatotb where idContato = '{$idContato}'";
+function buscarContatoID($conexao, $idContato){
+    $query = "Select * from contatostb where idContato = '{$idContato}'";
     $result = mysqli_query($conexao,$query);
+    $result = mysqli_fetch_array($result);
     return $result;
 }
 
 function alterarContato($conexao,$idContato,$nomeContato,$foneContato){
-    $query = "update contatotb set nomeContato='{$nomeContato}', 
+    $query = "update contatostb set nomeContato='{$nomeContato}', 
     foneContato='{$foneContato}' where idContato = '{$idContato}'";
     $result = mysqli_query($conexao,$query);
     return $result;
 }
 
 function deletarContato($conexao,$idContato){
-    $query = "Delete from contatotb where idContato = '{$idContato}'";
+    $query = "Delete from contatostb where idContato = '{$idContato}'";
     $result = mysqli_query($conexao,$query);
     return $result;
 }
